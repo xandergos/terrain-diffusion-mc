d9ee1819817149beff0a767d88da82f5ae116df0
joshua <joshuavanrooij@gmail.com>
2026-05-01 16:39:50 +0200

    Stop overriding minecraft's native spawn location selector

--- src/main/java/com/github/xandergos/terraindiffusionmc/mixin/MinecraftServerMixin.java
+++ src/main/java/com/github/xandergos/terraindiffusionmc/mixin/MinecraftServerMixin.java
@@ -9,6 +9,7 @@ import net.minecraft.util.math.ChunkPos;
 import net.minecraft.world.WorldProperties.SpawnPoint;
 import net.minecraft.world.chunk.ChunkLoadProgress;
 import net.minecraft.world.level.ServerWorldProperties;
+import net.minecraft.world.Heightmap;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -26,7 +27,7 @@ public class MinecraftServerMixin {
 
         // Calculate your target coordinates
         // This method must block until the terrain model returns the desired X/Z.
-        ChunkPos targetChunk = SpawnSelector.calculateCoarseSpawnChunk();
+        ChunkPos targetChunk = SpawnSelector.calculateCoarseSpawnChunk(world);
 
         int targetX = targetChunk.getCenterX();
         int targetZ = targetChunk.getCenterZ();
@@ -34,8 +35,11 @@ public class MinecraftServerMixin {
         // Use Vanilla logic to find a safe block within that area
         BlockPos safeSpawnPos = SpawnLocating.findServerSpawnPoint(world, new ChunkPos(targetChunk.x, targetChunk.z));
 
-        int spawnY = world.getTopY(net.minecraft.world.Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, targetX, targetZ);
-        safeSpawnPos = new BlockPos(targetX, spawnY, targetZ);
+        // Fallback to middle of the block if finding a saf spawn position fails.
+        if (safeSpawnPos == null) {
+            int spawnY = world.getTopY(Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, targetX, targetZ);
+            safeSpawnPos = new BlockPos(targetX, spawnY, targetZ);
+        }
 
         worldProperties.setSpawnPoint(SpawnPoint.create(world.getRegistryKey(), safeSpawnPos, 0.0F, 0.0F));
         ci.cancel();
